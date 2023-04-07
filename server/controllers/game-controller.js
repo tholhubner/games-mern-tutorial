bd4f27f519d6c79ce20cc3ca1bf2bb512c9d8702
Tyler Holhubner <tholhubner@gmail.com>
2023-04-07 03:49:06 -0400

    createGame controller complete, needs tested

--- server/controllers/game-controller.js
+++ server/controllers/game-controller.js
@@ -1,7 +1,42 @@
 const Game = require("../models/Game")
 
 createGame = (request, response) => {
-
+	try {
+		const requestBody = request.body
+		if (!requestBody) {
+			return response.status(400).json({
+				success: false,
+				error: "Please provide a game to create..."
+			})
+		}
+		const game = new Game(body)
+		if (!game) {
+			return response.status(400).json({
+				success: false,
+				error: err,
+			})
+		}
+		game.save()
+			.then(() => {
+				return response.status(200).json({
+					success: true,
+					id: game._id,
+					message: "Game successfully added to DB!"
+				})
+				.catch(error => {
+					return response.status(400).json({
+						success: false,
+						error,
+						message: "Game creation failed, due to following error..."
+					})
+				})
+			})
+	} catch (error) {
+		return response.status(400).json({
+			success: false,
+			error
+		})
+	}
 }
 
 getGameById = (request, response) => {
