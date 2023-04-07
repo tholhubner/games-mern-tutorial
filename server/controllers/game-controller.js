5f5c554e0181872da9f6ce9ea7221af9cfe2c948
Tyler Holhubner <tholhubner@gmail.com>
2023-04-07 04:11:05 -0400

    getGameById complete, needs tested. Small code clean up

--- server/controllers/game-controller.js
+++ server/controllers/game-controller.js
@@ -6,9 +6,10 @@ createGame = (request, response) => {
 		if (!requestBody) {
 			return response.status(400).json({
 				success: false,
-				error: "Please provide a game to create..."
+				error: "Please provide a game to create...",
 			})
 		}
+
 		const game = new Game(body)
 		if (!game) {
 			return response.status(400).json({
@@ -16,31 +17,50 @@ createGame = (request, response) => {
 				error: err,
 			})
 		}
+
 		game.save()
 			.then(() => {
 				return response.status(200).json({
 					success: true,
 					id: game._id,
-					message: "Game successfully added to DB!"
+					message: "Game successfully added to DB!",
 				})
 				.catch(error => {
 					return response.status(400).json({
 						success: false,
 						error,
-						message: "Game creation failed, due to following error..."
+						message: "Game creation failed, due to following error...",
 					})
 				})
 			})
 	} catch (error) {
 		return response.status(400).json({
 			success: false,
-			error
+			error,
 		})
 	}
 }
 
-getGameById = (request, response) => {
-
+getGameById = async (request, response) => {
+	try {
+		const game = await Game.findById({ _id: request.params.id })
+		if (!game) {
+			return response.status(404).json({
+				success: false,
+				error: "No game found with that id",
+			})
+		}
+		
+		return response.status(200).json({
+			success: true,
+			values: game,
+		})
+	} catch (error) {
+		return response.status(500).json({
+			success: false,
+			message: error.message,
+		})
+	}
 }
 
 getAllGames = (request, response) => {
