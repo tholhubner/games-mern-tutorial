5b1e1da7b5ec7803145c98d11a555c35deefe8e4
Tyler Holhubner <tholhubner@gmail.com>
2023-04-07 03:39:22 -0400

    created controllers and skeleton of game controller file

---
+++ server/controllers/game-controller.js
@@ -0,0 +1,29 @@
+const Game = require("../models/Game")
+
+createGame = (request, response) => {
+
+}
+
+getGameById = (request, response) => {
+
+}
+
+getAllGames = (request, response) => {
+
+}
+
+updateGame = (request, response) => {
+
+}
+
+deleteGame = (request, response) => {
+
+}
+
+module.exports = {
+	createGame,
+	getGameById,
+	getAllGames,
+	updateGame,
+	deleteGame,
+}
